ee9aa9aa5824e5f93bee23403d103148fdda388b
cfanpnk <cfanpnk@gmail.com>
2017-02-27 20:52:33 -0600

    Added

---
+++ exercises/small_problems_101/medium_1/test.rb
@@ -0,0 +1,43 @@
+PRODUCTS = [
+  { name: "Thinkpad x210", price: 220 },
+  { name: "Thinkpad x220", price: 250 },
+  { name: "Thinkpad x250", price: 979 },
+  { name: "Thinkpad x230", price: 300 },
+  { name: "Thinkpad x230", price: 330 },
+  { name: "Thinkpad x230", price: 350 },
+  { name: "Thinkpad x240", price: 700 },
+  { name: "Macbook Leopard", price: 300 },
+  { name: "Macbook Air", price: 700 },
+  { name: "Macbook Pro", price: 600 },
+  { name: "Macbook", price: 1449 },
+  { name: "Dell Latitude", price: 200 },
+  { name: "Dell Latitude", price: 650 },
+  { name: "Dell Inspiron", price: 300 },
+  { name: "Dell Inspiron", price: 450 }  
+]
+
+query = {
+  price_min: 240,
+  price_max: 280,
+  q: "thinkpad"
+}
+
+query2 = {
+  price_min: 300,
+  price_max: 450,
+  q: 'dell'
+}
+
+def search(query)
+  PRODUCTS.select do |product|
+    product[:name].downcase.include?(query[:q]) and product[:price] >= query[:price_min] and product[:price] <= query[:price_max]
+  end
+end
+
+p search(query)
+# [ { name: "Thinkpad x220", price: 250 }]
+
+p search(query2)
+# [ { name: "Dell Inspiron", price: 300 },
+#   { name: "Dell Inspiron", price: 450 }]
+
