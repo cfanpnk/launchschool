d0b3ea43db254ccfb263a28247c0ac63051d334b
cfanpnk <cfanpnk@gmail.com>
2017-03-26 15:01:28 -0500

    Added

---
+++ exercises/ruby_more_topics/easy_1/one.rb
@@ -0,0 +1,18 @@
+def one?(array)
+  flag = false
+  array.each do |element|
+    next if yield(element) == false
+    return false if flag
+    flag = true
+  end
+  flag
+end
+
+
+puts one?([1, 3, 5, 6]) { |value| value.even? }    # -> true
+puts one?([1, 3, 5, 7]) { |value| value.odd? }     # -> false
+puts one?([2, 4, 6, 8]) { |value| value.even? }    # -> false
+puts one?([1, 3, 5, 7]) { |value| value % 5 == 0 } # -> true
+puts one?([1, 3, 5, 7]) { |value| true }           # -> false
+puts one?([1, 3, 5, 7]) { |value| false }          # -> false
+puts one?([]) { |value| true }                     # -> false
