161c0426f518e40140f03d6828d239c3d30320b5
cfanpnk <cfanpnk@gmail.com>
2017-03-30 08:31:05 -0500

    Added

---
+++ exercises/ruby_more_topics/easy_2/drop_while.rb
@@ -0,0 +1,20 @@
+
+def drop_while(array)
+  index = 0
+  loop do
+    break if index > array.size - 1
+    if yield(array[index]) == true
+      index += 1
+    else
+      break
+    end
+  end
+  array[index..-1]
+end
+
+puts drop_while([1, 3, 5, 6]) { |value| value.odd? } == [6]
+puts drop_while([1, 3, 5, 6]) { |value| value.even? } == [1, 3, 5, 6]
+drop_while([1, 3, 5, 6]) { |value| true } == []
+puts drop_while([1, 3, 5, 6]) { |value| false } == [1, 3, 5, 6]
+puts drop_while([1, 3, 5, 6]) { |value| value < 5 } == [5, 6]
+puts drop_while([]) { |value| true } == []
