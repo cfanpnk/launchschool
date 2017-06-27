eecc883cfce8c70a687be9a055fdd06e7bf87da5
cfanpnk <cfanpnk@gmail.com>
2017-06-27 08:31:39 -0500

    Added

---
+++ backend/ruby_foundations/exercises/small_problems_101/medium_1/fibonacci_last_digit.rb
@@ -0,0 +1,15 @@
+def fibonacci_last(num)
+  result = [1, 1]
+  (3..num % 60).each do 
+    result = [result.last, (result.last + result.first) % 10]
+  end
+  result.last
+end
+
+
+p fibonacci_last(15)        # -> 0  (the 15th Fibonacci number is 610)
+p fibonacci_last(20)        # -> 5 (the 20th Fibonacci number is 6765)
+p fibonacci_last(100)       # -> 5 (the 100th Fibonacci number is 354224848179261915075)
+p fibonacci_last(100_001)   # -> 1 (this is a 20899 digit number)
+p fibonacci_last(1_000_007) # -> 3 (this is a 208989 digit number)
+p fibonacci_last(123456789) # -> 4
